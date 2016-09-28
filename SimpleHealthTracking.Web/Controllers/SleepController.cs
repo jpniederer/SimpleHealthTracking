ed9519f3a6ee350eb8fcecdf7d713538bd11062d
Joshua Niederer <josh@jpniederer.com>
2016-09-27 23:11:21 -0500

    Adding some basic styles. Editing the SleepController.Create functionality.

--- SimpleHealthTracking.Web/Controllers/SleepController.cs
+++ SimpleHealthTracking.Web/Controllers/SleepController.cs
@@ -1,5 +1,10 @@
 ﻿namespace SimpleHealthTracking.Web.Controllers
 {
+    using SimpleHealthTracking.Repository;
+    using SimpleHealthTracking.Repository.Entities;
+    using SimpleHealthTracking.Repository.Factories;
+    using SimpleHealthTracking.Web.ViewModels;
+    using Microsoft.AspNet.Identity;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -8,10 +13,74 @@
 
     public class SleepController : Controller
     {
-        // GET: SleepView
+        ISimpleHealthTrackerRepository repository;
+        SleepFactory sleepFactory = new SleepFactory();
+
+        public SleepController()
+        {
+            repository = new SimpleHealthTrackerRepository(new SimpleHealthTrackerContext());
+        }
+
+        public SleepController(ISimpleHealthTrackerRepository repo)
+        {
+            repository = repo;
+        }
+        
+        [Authorize]
         public ActionResult Create()
         {
-            return View();
+            SleepViewModel viewModel = new SleepViewModel
+            {
+
+            };
+
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SleepViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", viewModel);
+            }
+
+            Sleep sleep = new Sleep
+            {
+                UserId = User.Identity.GetUserId(),
+                StartTime = viewModel.GetStartDateTime(),
+                EndTime = viewModel.GetEndDateTime(),
+                SleepQuality = viewModel.GetSleepQuality(),
+                TimeAdded = DateTime.Now,
+                UpdateTime = DateTime.Now
+            };
+
+            sleep.MinutesSlept = GetMinutesSlept(sleep);
+
+            repository.InsertSleep(sleep);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private float GetMinutesSlept(Sleep sleep)
+        {
+            long startTicks = 0;
+            long endTicks = 0;
+
+            if (sleep.EndTime != null && sleep.StartTime != null)
+            {
+                startTicks = ((DateTime)sleep.StartTime).Ticks;
+                endTicks = ((DateTime)sleep.EndTime).Ticks;
+            }
+
+            if (endTicks > startTicks)
+            {
+                return (float)TimeSpan.FromTicks(endTicks - startTicks).TotalMinutes;
+            }
+
+            return 0.0f;
         }
     }
 }
---
+++ SimpleHealthTracking.Web/ViewModels/SleepViewModel.cs
@@ -0,0 +1,50 @@
+﻿namespace SimpleHealthTracking.Web.ViewModels
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class SleepViewModel
+    {
+        [Required]
+        [ValidDate]
+        public string StartDate { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string StartTime { get; set; }
+
+        [Required]
+        [ValidDate]
+        public string EndDate { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string EndTime { get; set; }
+
+        public string SleepQuality { get; set; }
+
+        public DateTime GetStartDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", StartDate, StartTime));
+        }
+
+        public DateTime GetEndDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", EndDate, EndTime));
+        }
+
+        public float? GetSleepQuality()
+        {
+            float val;
+
+            if (float.TryParse(SleepQuality, out val))
+            {
+                return val;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
