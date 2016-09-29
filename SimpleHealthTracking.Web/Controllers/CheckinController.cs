f6d4cb2f1cb7b9742e9f687c96779c59c0b1bc02
Joshua Niederer <josh@jpniederer.com>
2016-09-28 20:17:12 -0500

    Updated the Checkin creation form.

--- SimpleHealthTracking.Web/Controllers/CheckinController.cs
+++ SimpleHealthTracking.Web/Controllers/CheckinController.cs
@@ -1,5 +1,10 @@
 ﻿namespace SimpleHealthTracking.Web.Controllers
 {
+    using SimpleHealthTracking.Repository;
+    using SimpleHealthTracking.Repository.Entities;
+    using SimpleHealthTracking.Repository.Factories;
+    using SimpleHealthTracking.Web.ViewModels;
+    using Microsoft.AspNet.Identity;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -8,10 +13,54 @@
 
     public class CheckinController : Controller
     {
-        // GET: CheckinView
+        ISimpleHealthTrackerRepository repository;
+        CheckinFactory checkinFactory = new CheckinFactory();
+
+        public CheckinController()
+        {
+            repository = new SimpleHealthTrackerRepository(new SimpleHealthTrackerContext());
+        }
+        
+        public CheckinController(ISimpleHealthTrackerRepository repo)
+        {
+            repository = repo;
+        }
+
+        [Authorize]
         public ActionResult Create()
         {
-            return View();
+            CheckinViewModel viewModel = new CheckinViewModel();
+
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CheckinViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", viewModel);
+            }
+
+            Checkin checkin = new Checkin
+            {
+                UserId = User.Identity.GetUserId(),
+                Weight = viewModel.GetFloatValue(viewModel.Weight),
+                Heartrate = viewModel.GetFloatValue(viewModel.Heartrate),
+                SystolicBloodPressure = viewModel.GetFloatValue(viewModel.SystolicBloodPressure),
+                DiastolicBloodPressure = viewModel.GetFloatValue(viewModel.DiastolicBloodPressure),
+                PhysicalFeelingRating = viewModel.GetFloatValue(viewModel.PhysicalFeelingRating),
+                PsychologicalFeelingRating = viewModel.GetFloatValue(viewModel.PsychologicalFeelingRating),
+                ExerciseRating = viewModel.GetFloatValue(viewModel.ExerciseRating),
+                TimeAdded = DateTime.Now,
+                UpdateTime = DateTime.Now
+            };
+
+            repository.InsertCheckin(checkin);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ SimpleHealthTracking.Web/ViewModels/CheckinViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleHealthTracking.Web.ViewModels
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class CheckinViewModel
+    {
+        public string Weight { get; set; }
+        public string Heartrate { get; set; }
+        public string SystolicBloodPressure { get; set; }
+        public string DiastolicBloodPressure { get; set; }
+        public string PhysicalFeelingRating { get; set; }
+        public string PsychologicalFeelingRating { get; set; }
+        public string ExerciseRating { get; set; }
+        public string Notes { get; set; }
+
+        public float? GetFloatValue(string possibleValue)
+        {
+            float value;
+
+            if (float.TryParse(possibleValue, out value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+    }
+}
