63b488393f2405072827b4f94b95812b370dd604
Joshua Niederer <josh@jpniederer.com>
2016-10-03 20:58:25 -0500

    Updating the Medicine controller to add basic operations.

--- SimpleHealthTracking.Web/Controllers/MedicineController.cs
+++ SimpleHealthTracking.Web/Controllers/MedicineController.cs
@@ -10,6 +10,7 @@
     using System.Linq;
     using System.Web;
     using System.Web.Mvc;
+    using System.Net;
 
     public class MedicineController : Controller
     {
@@ -59,6 +60,110 @@
             repository.InsertMedicine(medicine);
 
             return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var currentUser = User.Identity.GetUserId();
+            Medicine medicine = repository.GetMedicine(id);
+
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (currentUser != medicine.UserId)
+            {
+                return new HttpUnauthorizedResult();
+            }
+
+            return View(medicine);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Medicine medicine)
+        {
+            var currentUser = User.Identity.GetUserId();
+            medicine.UpdateTime = DateTime.Now;
+
+            if (currentUser != medicine.UserId)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            }
+
+            if (ModelState.IsValid)
+            {
+                repository.UpdateMedicine(medicine);
+                return RedirectToAction("Index");
+            }
+
+            return View(medicine);
+        }
+
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            var currentUser = User.Identity.GetUserId();
+            Medicine medicine = repository.GetMedicine(id);
+
+            if (currentUser != medicine.UserId)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            }
+
+            if (medicine == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View(medicine);
+        }
+
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var currentUser = User.Identity.GetUserId();
+            Medicine medicine = repository.GetMedicine(id);
+
+            if (currentUser != medicine.UserId)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            }
+
+            repository.DeleteMedicine(id);
+
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public ActionResult Details(int id)
+        {
+            var currentUser = User.Identity.GetUserId();
+            Medicine medicine = repository.GetMedicine(id);
+
+            if (currentUser != medicine.UserId)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            }
+
+            if (medicine == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View(medicine);
+        }
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            var currentUser = User.Identity.GetUserId();
+            return View(repository.GetMedicinesForUser(currentUser).ToList());
         }
     }
 }
