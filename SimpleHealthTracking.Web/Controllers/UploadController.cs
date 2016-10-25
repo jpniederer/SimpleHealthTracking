7c065c3267403fea9077b140245723d77b555d53
Josh Niederer <josh@jpniederer.com>
2016-10-25 17:49:09 -0500

    Starting on an upload Excel feature directly in the MVC app. I plan to use ExcelDataReader. This way will be cleaner than using Python to the Web API methods.

---
+++ SimpleHealthTracking.Web/Controllers/UploadController.cs
@@ -0,0 +1,38 @@
+﻿namespace SimpleHealthTracking.Web.Controllers
+{
+    using Excel;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class UploadController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult UploadExcel()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadExcel(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // Process file.
+            }
+
+            return RedirectToAction("UploadExcel");
+        }
+
+    }
+}
