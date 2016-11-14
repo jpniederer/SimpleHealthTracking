03a887789fcd51d23573ab87a4574ca6d2d8ade9
Joshua Niederer <josh@jpniederer.com>
2016-11-13 18:49:58 -0600

    Starting on a class that can get standard health statistics.

---
+++ SimpleHealthTracking.Tests/Web/AverageCalculationsTest.cs
@@ -0,0 +1,31 @@
+﻿namespace SimpleHealthTracking.Tests.Web
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Repository.Entities;
+    using SimpleHealthTracking.Web.Classes;
+
+    [TestClass]
+    public class AverageCalculationsTest
+    {
+        string userId = "1f19feb6-3a7d-4903-bdeb-610826f779bb";
+
+        [TestMethod]
+        public void TestAverageWeight()
+        {
+            HealthStatistics hs = new HealthStatistics(userId);
+            double averageWeight = hs.GetAverageWeight();
+
+            Assert.AreNotEqual(0, averageWeight);
+        }
+
+        [TestMethod]
+        public void TestMaxWeight()
+        {
+            HealthStatistics hs = new HealthStatistics(userId);
+            double maxWeight = hs.GetMaxWeight();
+
+            Assert.AreNotEqual(0, maxWeight);
+        }
+    }
+}
---
+++ SimpleHealthTracking.Web/Classes/HealthStatistics.cs
@@ -0,0 +1,86 @@
+﻿namespace SimpleHealthTracking.Web.Classes
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Repository;
+    using Repository.Entities;
+
+    public class HealthStatistics
+    {
+        List<Checkin> checkins;
+        List<MedicineTaken> medicinesTaken;
+        List<Sleep> sleeps;
+        ISimpleHealthTrackerRepository repository;
+        string userId;
+
+        public HealthStatistics(string user)
+        {
+            userId = user;
+            repository = new SimpleHealthTrackerRepository(new SimpleHealthTrackerContext());
+        }
+
+        public HealthStatistics(ISimpleHealthTrackerRepository repo, string user)
+        {
+            repository = repo;
+            userId = user;
+        }
+
+        private void SetupCheckins()
+        {
+            checkins = repository.GetCheckinsForUser(userId).ToList();
+        }
+
+        private void SetupMedicinesTakens()
+        {
+            medicinesTaken = repository.GetMedicineTakenByUser(userId).ToList();
+        }
+
+        private void SetupSleeps()
+        {
+            sleeps = repository.GetSleepForUser(userId).ToList();
+        }
+
+        public double GetAverageWeight()
+        {
+            double averageWeight = 0.0;
+
+            if (checkins == null)
+            {
+                SetupCheckins();
+            }
+
+            averageWeight = checkins.Where(c => c.Weight != null).Average(c => (float)c.Weight);
+
+            return averageWeight;
+        }
+
+        public double GetAverageWeightFromLatestNumberOfEntries(int numberEntries)
+        {
+            double averageWeight = 0.0;
+
+            if (checkins == null)
+            {
+                SetupCheckins();
+            }
+
+            averageWeight = checkins.Where(c => c.Weight != null).Take(numberEntries)
+                            .Average(c => (float)c.Weight);
+
+            return averageWeight;
+        }
+
+        public double GetMaxWeight()
+        {
+            double maxWeight = 0.0;
+
+            if (checkins == null)
+            {
+                SetupCheckins();
+            }
+
+            maxWeight = checkins.Where(c => c.Weight != null).Max(c => (float)c.Weight);
+
+            return maxWeight;
+        }
+    }
+}
