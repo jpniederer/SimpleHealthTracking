227f7f30a9d6a4eee13b357358c01519da660eb7
Josh Niederer <josh@jpniederer.com>
2016-11-20 22:36:51 -0600

    Filling out the stats API controller.

--- SimpleHealthTracking.Web/Controllers/StatsApiController.cs
+++ SimpleHealthTracking.Web/Controllers/StatsApiController.cs
@@ -34,5 +34,70 @@
 
             return Json(maxHeartrateCheckin);
         }
+
+        [Authorize]
+        [HttpGet]
+        public IHttpActionResult GetAverageWeight()
+        {
+            string userId = User.Identity.GetUserId();
+            HealthStatistics hs = new HealthStatistics(userId);
+            hs.GetAverageWeight();
+
+            return Json(hs.AverageWeight);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IHttpActionResult GetAverageHeartrate()
+        {
+            string userId = User.Identity.GetUserId();
+            HealthStatistics hs = new HealthStatistics(userId);
+            hs.GetAverageHeartrate();
+
+            return Json(hs.AverageHeartrate);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IHttpActionResult GetAverageMinutesSlept()
+        {
+            string userId = User.Identity.GetUserId();
+            HealthStatistics hs = new HealthStatistics(userId);
+            hs.GetAverageMinutesSlept();
+
+            return Json(hs.AverageMinutesSlept);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IHttpActionResult GetAverageSleepStartTime()
+        {
+            string userId = User.Identity.GetUserId();
+            HealthStatistics hs = new HealthStatistics(userId);
+            hs.SetAverageStartAndEndSleepTimes();
+
+            return Json(hs.AverageSleepStartTime);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IHttpActionResult GetAverageSleepEndTime()
+        {
+            string userId = User.Identity.GetUserId();
+            HealthStatistics hs = new HealthStatistics(userId);
+            hs.SetAverageStartAndEndSleepTimes();
+
+            return Json(hs.AverageSleepEndTime);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IHttpActionResult GetHealthStats()
+        {
+            string userId = User.Identity.GetUserId();
+            HealthStatistics hs = new HealthStatistics(userId, true);
+
+            return Json(hs);
+        }
     }
 }
