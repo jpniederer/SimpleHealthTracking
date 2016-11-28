ce76749ea04b573c581031bd93e3a7ca4c688ac1
Joshua Niederer <josh@jpniederer.com>
2016-11-27 21:59:08 -0600

    Generalizing the home page graph functions into one function.

--- SimpleHealthTracking.Web/Scripts/charts.js
+++ SimpleHealthTracking.Web/Scripts/charts.js
@@ -5,20 +5,75 @@ function setupDataSetsHome() {
         checkinWeightsJson = d;
     })
     .done(function () {
-        setupWeightGraphHome();
+        homeGraphGeneral(setupWeights, "#weightChart", 1, '');
     });
     $.get("api/CheckinApi/GetLastCheckinsForHeartrates?count=30", function (d) {
         checkinHeartratesJson = d;
     })
     .done(function () {
-        setupHeartrateGraphHome();
+        homeGraphGeneral(setupHeartrates, "#heartChart", 5, '');
     });
     $.get("api/SleepApi/GetLastFullSleeps?count=30", function (d) {
         sleepTimesJson = d;
     })
     .done(function () {
-        setupSleepGraphHome();
+        homeGraphGeneral(setupSleeps, "#sleepChart", 0.1, '');
     });
+};
+
+function homeGraphGeneral(setupFunction, idName, scaleModifier, name) {
+    var items = setupFunction();
+    var parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S");
+    var graph = d3.select(idName),
+        margins = { top: 20, right: 20, bottom: 20, left: 50 },
+        width = 400,
+        height = 400,
+        // d3 converts local time to GMT.
+        xRange = d3.scaleTime().range([margins.left, width - margins.right]).domain([
+            d3.min(items, function (d) {
+                return parseTime(d.x);
+            }),
+            d3.max(items, function (d) {
+                return parseTime(d.x);
+            })
+        ]),
+        yRange = d3.scaleLinear().range([height - margins.top, margins.bottom]).domain([
+            d3.min(items, function (d) {
+                return d.y - scaleModifier;
+            }),
+            d3.max(items, function (d) {
+                return d.y + scaleModifier;
+            })
+        ]),
+        xAxis = d3.axisBottom(xRange)
+                .ticks(5)
+                .tickFormat(d3.timeFormat("%m/%d")),
+        yAxis = d3.axisLeft(yRange);
+
+    graph.append("svg:g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + (height - margins.bottom) + ")")
+        .call(xAxis);
+
+    graph.append("svg:g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + (margins.left) + ",0)")
+      .call(yAxis);
+
+    var lineFunc = d3.line()
+        .x(function (d) {
+            return xRange(parseTime(d.x));
+        })
+        .y(function (d) {
+            return yRange(d.y);
+        });
+
+    graph.append('svg:path')
+        .attr('d', lineFunc(items))
+        .attr('stroke', 'blue')
+        .attr('stroke-width', 2)
+        .attr('fill', 'none')
+        .text(name)
 };
 
 function setupSleeps() {
@@ -32,61 +87,6 @@ function setupSleeps() {
     return sleeps;
 };
 
-function setupSleepGraphHome() {
-    var sleeps = setupSleeps();
-    var parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S");
-    var weight = d3.select("#sleepChart"),
-        margins = { top: 20, right: 20, bottom: 20, left: 50 },
-        width = 400,
-        height = 400,
-        xRange = d3.scaleTime().range([margins.left, width - margins.right]).domain([
-            d3.min(sleeps, function (d) {
-                return parseTime(d.x);
-            }),
-            d3.max(sleeps, function (d) {
-                return parseTime(d.x);
-            })
-        ]),
-        yRange = d3.scaleLinear().range([height - margins.top, margins.bottom]).domain([
-            d3.min(sleeps, function (d) {
-                return d.y - 0.1;
-            }),
-            d3.max(sleeps, function (d) {
-                return d.y + 0.1;
-            })
-        ]),
-        xAxis = d3.axisBottom(xRange)
-                .ticks(5)
-                .tickFormat(d3.timeFormat("%m/%d")),
-        yAxis = d3.axisLeft(yRange);
-
-
-    weight.append("svg:g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + (height - margins.bottom) + ")")
-        .call(xAxis);
-
-    weight.append("svg:g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + (margins.left) + ",0)")
-      .call(yAxis);
-
-    var lineFunc = d3.line()
-        .x(function (d) {
-            return xRange(parseTime(d.x));
-        })
-        .y(function (d) {
-            return yRange(d.y);
-        });
-
-    weight.append('svg:path')
-        .attr('d', lineFunc(sleeps))
-        .attr('stroke', 'blue')
-        .attr('stroke-width', 2)
-        .attr('fill', 'none')
-        .text("Recent Heartrates")
-};
-
 function setupWeights() {
     var weights = checkinWeightsJson.map(function (item) {
         return {
@@ -98,61 +98,6 @@ function setupWeights() {
     return weights;
 };
 
-function setupWeightGraphHome() {
-    var weights = setupWeights();
-    // d3 converts local time to GMT.
-    var parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S");
-    var weight = d3.select("#weightChart"),
-        margins = { top: 20, right: 20, bottom: 20, left: 50 },
-        width = 400,
-        height = 400,
-        xRange = d3.scaleTime().range([margins.left, width - margins.right]).domain([
-            d3.min(weights, function (d) {
-                return parseTime(d.x);
-            }),
-            d3.max(weights, function (d) {
-                return parseTime(d.x);
-            })
-        ]),
-        yRange = d3.scaleLinear().range([height - margins.top, margins.bottom]).domain([
-            d3.min(weights, function (d) {
-                return d.y - 1;
-            }),
-            d3.max(weights, function (d) {
-                return d.y + 1;
-            })
-        ]),
-        xAxis = d3.axisBottom(xRange)
-                .ticks(5)
-                .tickFormat(d3.timeFormat("%m/%d")),
-        yAxis = d3.axisLeft(yRange);
-
-    weight.append("svg:g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + (height - margins.bottom) + ")")
-        .call(xAxis);
-
-    weight.append("svg:g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + (margins.left) + ",0)")
-      .call(yAxis);
-
-    var lineFunc = d3.line()
-        .x(function (d) {
-            return xRange(parseTime(d.x));
-        })
-        .y(function (d) {
-            return yRange(d.y);
-        });
-
-    weight.append('svg:path')
-        .attr('d', lineFunc(weights))
-        .attr('stroke', 'blue')
-        .attr('stroke-width', 2)
-        .attr('fill', 'none')
-        .text("Recent Heartrates")
-};
-
 function setupHeartrates() {
     var heartrates = checkinHeartratesJson.map(function (item) {
         return {
@@ -162,60 +107,4 @@ function setupHeartrates() {
     });
 
     return heartrates;
-};
-
-function setupHeartrateGraphHome() {
-    var heartrates = setupHeartrates();
-    // d3 converts local time to GMT.
-    var parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S");
-    var heart = d3.select("#heartChart"),
-        margins = { top: 20, right: 20, bottom: 20, left: 50 },
-        width = 400, //+.attr("width") - margin.left - margin.right,
-        height = 400, //+svg.attr("height") - margin.top - margin.bottom,
-        //g = svg.append("g").attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
-        xRange = d3.scaleTime().range([margins.left, width - margins.right]).domain([
-            d3.min(heartrates, function (d) {
-                return parseTime(d.x);
-            }),
-            d3.max(heartrates, function (d) {
-                return parseTime(d.x);
-            })
-        ]),
-        yRange = d3.scaleLinear().range([height - margins.top, margins.bottom]).domain([
-            d3.min(heartrates, function (d) {
-                return d.y - 5;
-            }),
-            d3.max(heartrates, function (d) {
-                return d.y + 5;
-            })
-        ]),
-        xAxis = d3.axisBottom(xRange)
-                .ticks(5)
-                .tickFormat(d3.timeFormat("%m/%d")),
-        yAxis = d3.axisLeft(yRange);
-
-    heart.append("svg:g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + (height - margins.bottom) + ")")
-        .call(xAxis);
-
-    heart.append("svg:g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + (margins.left) + ",0)")
-      .call(yAxis);
-
-    var lineFunc = d3.line()
-        .x(function (d) {
-            return xRange(parseTime(d.x));
-        })
-        .y(function (d) {
-            return yRange(d.y);
-        });
-
-    heart.append('svg:path')
-        .attr('d', lineFunc(heartrates))
-        .attr('stroke', 'blue')
-        .attr('stroke-width', 2)
-        .attr('fill', 'none')
-        .text("Recent Heartrates")
 };
