ce02c9652e11423095a19fedc7446c2fb6f9c0e5
Joshua Niederer <josh@jpniederer.com>
2016-12-01 21:50:59 -0600

    Starting on Medicine/Streak stat functionality.

---
+++ SimpleHealthTracking.Web/Classes/MedicineStats.cs
@@ -0,0 +1,57 @@
+﻿namespace SimpleHealthTracking.Web.Classes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Repository;
+    using Repository.Entities;
+
+    public class MedicineStats
+    {
+        public Medicine Medicine { get; set; }
+        public List<MedicineTaken> MedicinesTaken { get; }
+        public int CompletionCount { get; private set; }
+        public int TotalExpected { get; private set; }
+        public double CompletionPercentage { get; private set; }
+        public bool DoesMedicineHaveRecords { get; private set; }
+        ISimpleHealthTrackingRepository repository;
+
+        public MedicineStats(Medicine medicine)
+        {
+            repository = new SimpleHealthTrackingRepository(new SimpleHealthTrackerContext());
+            Medicine = medicine;
+            MedicinesTaken = repository.GetMedicineTakenByMedicineId(Medicine.Id).ToList();
+
+            if (MedicinesTaken != null && MedicinesTaken.Count > 0)
+            {
+                DoesMedicineHaveRecords = true;
+                SetCompletionCount();
+                SetExpectedTotal();
+                SetCompletionPercentage();
+            }
+            else
+            {
+                DoesMedicineHaveRecords = false;
+            }
+        }
+
+        private void SetCompletionCount()
+        {
+            CompletionCount = MedicinesTaken.Count();
+        }
+
+        private void SetExpectedTotal()
+        {
+            DateTime firstDay = MedicinesTaken.Min(mt => mt.DateAddedFor);
+            long dayCount = ((DateTime.Now.Ticks - firstDay.Ticks) / TimeSpan.TicksPerDay) + 1;
+            TotalExpected = (int)dayCount * Medicine.NumberOfTimesPerDay;
+        }
+
+        private void SetCompletionPercentage()
+        {
+            CompletionPercentage = 100 * ((double)CompletionCount / TotalExpected);
+        }
+    }
+}
