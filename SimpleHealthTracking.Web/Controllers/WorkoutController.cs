a786ef516e2555bf903530b18fcc6e46dfb40c46
Joshua Niederer <josh@jpniederer.com>
2017-02-08 21:10:31 -0600

    Starting on the WorkoutController.

---
+++ SimpleHealthTracking.Web/Controllers/WorkoutController.cs
@@ -0,0 +1,46 @@
+﻿namespace SimpleHealthTracking.Web.Controllers
+{
+    using Repository;
+    using Repository.Entities;
+    using Microsoft.AspNet.Identity;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class WorkoutController : Controller
+    {
+        ISimpleHealthTrackingRepository repository;
+
+        public WorkoutController()
+        {
+            repository = new SimpleHealthTrackingRepository(new SimpleHealthTrackingContext());
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Workout workout)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", workout);
+            }
+
+            repository.InsertWorkout(workout);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
